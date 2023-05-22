b56bba3fb9e2d05297b16c60fcd25f9d43a6830a
raji <raji@gmail.com>
2023-05-22 06:11:07 +0000

    Solution

---
+++ src/components/About/index.js
@@ -0,0 +1,18 @@
+import './index.css'
+
+const About = () => (
+  <div className="home-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/about-sm-img.png"
+      alt="about"
+      className="mobile-image"
+    />
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/about-lg-img.png"
+      alt="about"
+      className="large-device-image"
+    />
+  </div>
+)
+
+export default About
---
+++ src/components/Home/index.js
@@ -0,0 +1,18 @@
+import './index.css'
+
+const Home = () => (
+  <div className="home-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/home-sm-img.png"
+      alt="home"
+      className="mobile-image"
+    />
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/home-lg-img.png"
+      alt="home"
+      className="large-device-image"
+    />
+  </div>
+)
+
+export default Home
